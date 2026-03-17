39d63ecad841cb9a8f830e9f1a371d39c8b141b1
Kylian Mathurin <kmathuri@c1r3p9.42nice.fr>
2026-03-17 13:48:18 +0100

    Fix: parsers block and first line

--- parser.py
+++ parser.py
@@ -39,23 +39,24 @@ class Parser:
     def process(self) -> list[ProcessedDict]:
         res = []
         parser = ParserManager()
-        blocks = self.content.split("\n\n")
+        lines = self.content.split("\n")
+        file_line = 1
         i = 0
-        for block in blocks:
-            block_content = []
-            lines = block.split("\n")
-            for line in lines:
-                if not line.startswith("#") and len(line):
-                    block_content.append(parser.process(line))
-            i += 1
-            res += block_content
+        for line in lines:
+            if not line.startswith("#") and len(line):
+                try:
+                    res.append(parser.process(line))
+                except ValidationError as e:
+                    raise ValueError(f"[{file_line}] - {e.errors()[0]['msg']}")
+                except Exception as e:
+                    raise ValueError(f"[{file_line}] - {e}")
+                i += 1
             if i == 1:
-                if block_content[0]["parser"] != "drone_count":
+                if res[0]["parser"] != "drone_count":
                     raise ValueError(
-                        "Number of drone must be at the first line of the file"
+                        f"[{file_line}] - Number of drone must be at the first line of the file"
                     )
-        if i != 3:
-            raise ValueError("Unexpected number of block")
+            file_line += 1
         return res
 
 
@@ -100,7 +101,7 @@ class StartHubParser(SpecificParser):
         if not match:
             raise ValueError("Error: Line malformed.")
         obj = match.groupdict()
-        obj.update({"color": None, "max_drones": -1})
+        obj.update({"color": None, "max_drones": -1, "start": True})
         obj["x"] = int(obj["x"])
         obj["y"] = int(obj["y"])
         if not len(obj["extra"]):
@@ -161,7 +162,7 @@ class EndHubParser(SpecificParser):
         if not match:
             raise ValueError("Error: Line malformed.")
         obj = match.groupdict()
-        obj.update({"color": None, "max_drones": -1})
+        obj.update({"color": None, "max_drones": -1, "end": True})
         obj["x"] = int(obj["x"])
         obj["y"] = int(obj["y"])
         if not len(obj["extra"]):
@@ -178,7 +179,11 @@ class EndHubParser(SpecificParser):
 class ConnectionParser(SpecificParser):
     def __init__(self) -> None:
         self._regex = re.compile(
-            r"^connection:\s+(?P<connection>\b\w+-\b\w+)", re.M
+            r"^connection:\s+(?P<connection>\b\w+-\b\w+)(?P<extra>.*)$", re.M
+        )
+        self._extra_regex = re.compile(
+            r"^connection:\s+(?P<connection>\b\w+-\b\w+)(?P<extra>.*)$",
+            re.M,
         )
 
     def process(self, line: str) -> dict:
@@ -186,6 +191,13 @@ class ConnectionParser(SpecificParser):
         if not match:
             raise ValueError("Error: Line malformed.")
         obj = match.groupdict()
+        obj.update({"max_link_capacity": 1})
+        extra = re.finditer(r"(?P<key>\w+)=(?P<value>\w+)", obj["extra"])
+        for m in extra:
+            obj[m.group("key")] = m.group("value")
+            if m.group("key") == "max_link_capacity":
+                obj[m.group("key")] = int(m.group("value"))
+        del obj["extra"]
         return obj
 
 
@@ -199,6 +211,8 @@ class StartOrEndHubValidator(BaseModel):
     y: int = Field(ge=0, le=40)
     color: Optional[str] = Field(default=None)
     max_drones: int = Field(ge=-1, le=-1)
+    start: bool = Field(default=False)
+    end: bool = Field(default=False)
 
     @model_validator(mode="after")
     def id_check(self: "StartOrEndHubValidator") -> "StartOrEndHubValidator":
@@ -260,6 +274,7 @@ class HubValidator(BaseModel):
 
 class ConnectionValidator(BaseModel):
     connection: str = Field(min_length=3)
+    max_link_capacity: int = Field(default=1)
 
     @model_validator(mode="after")
     def connection_check(
