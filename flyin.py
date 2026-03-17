9a92a6e9e97ad391dfd29178fd8efb65a1c7e54b
Kylian Mathurin <kmathuri@c1r3p9.42nice.fr>
2026-03-17 13:47:55 +0100

    Feature: graph basics

--- flyin.py
+++ flyin.py
@@ -1,14 +1,48 @@
+from pydantic import ValidationError
+from graph import Graph
 from parser import (
+    ConnectionValidator,
+    HubValidator,
     Parser,
+    StartOrEndHubValidator,
 )
 import sys
+
+from visualizer import Visualizer
 
 
 def main(argv: list[str]) -> None:
     parser = Parser()
     try:
         parser.open(argv[1])
-        print(parser.process())
+        try:
+            res = parser.process()
+            print(res)
+            graph = Graph("test")
+            graph.convert_to_graph(
+                [
+                    node["parsed_and_validated_data"]
+                    for node in res
+                    if isinstance(
+                        node["parsed_and_validated_data"],
+                        StartOrEndHubValidator,
+                    )
+                    or isinstance(
+                        node["parsed_and_validated_data"], HubValidator
+                    )
+                ],
+                [
+                    node["parsed_and_validated_data"]
+                    for node in res
+                    if isinstance(
+                        node["parsed_and_validated_data"], ConnectionValidator
+                    )
+                ],
+            )
+            visualizer = Visualizer(graph.nodes, graph.links)
+            visualizer.run()
+        except (ValidationError, ValueError) as e:
+            print("Error:", e)
     except (FileNotFoundError, PermissionError) as e:
         print("Error:", e)
 
